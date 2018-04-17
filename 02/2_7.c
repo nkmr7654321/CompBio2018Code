942a39a4a0ebbf043f1b43b9acd53f992dfa9443
nkmr7654321 <ashofabyss@gmail.com>
2018-04-17 12:30:26 +0900

    3

--- 02/2_7.c
+++ 02/2_7.c
@@ -4,12 +4,12 @@ int main(void){
   int t;
   double r,x,xx,k;
   FILE *fp;
-  x=1;
   k=100.0;
 
   fp=fopen("outputt.dat","a");
 
 for(r=1.0;r<3.0;r+=0.1){
+  x=1;
  for(t=250;t<300;t++){
   xx=x+r*(1-x/k)*x;
   fprintf(fp, "%d,%f\n",t,xx );
