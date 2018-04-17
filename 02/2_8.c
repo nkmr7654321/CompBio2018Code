91518380ee60025671e5276e8e54b8d353d5ef27
nkmr7654321 <ashofabyss@gmail.com>
2018-04-17 17:24:22 +0900

    2

---
+++ 02/2_8.c
@@ -0,0 +1,22 @@
+#include<stadio.h>
+
+int main(void){
+
+  double x=10,xx,r,k=100.0
+  int t;
+  FILE +fp:
+
+  r=3.0:
+
+  fp=fopen("outputtt.dat","w")
+  for(t=0;t<50;t++){
+    xx=x+r*(1.0-x/k)*x;
+    fprintf(fp, "%.1f\n",xx);
+    x=xx
+
+}
+fclose(fp);
+
+printf("COMPLETE\n")
+return 0;
+}
