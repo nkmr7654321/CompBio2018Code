8ba7e698685954fd96e439aea6ae9bafad23b785
nkmr7654321 <ashofabyss@gmail.com>
2018-04-17 17:29:02 +0900

    2

--- 02/2_8.c
+++ 02/2_8.c
@@ -4,19 +4,19 @@ int main(void){
 
   double x=10,xx,r,k=100.0;
   int t;
-  FILE +fp:
+  FILE +fp;
 
-  r=3.0:
+  r=3.0;
 
   fp=fopen("outputtt.dat","w")
   for(t=0;t<50;t++){
     xx=x+r*(1.0-x/k)*x;
     fprintf(fp, "%.1f\n",xx);
-    x=xx
+    x=xx;
 
 }
 fclose(fp);
 
-printf("COMPLETE\n")
+printf("COMPLETE\n");
 return 0;
 }
